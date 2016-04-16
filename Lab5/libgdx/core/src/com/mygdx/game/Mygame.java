57c89a6c9da3e1e15d1431d7577d7f0d37128740
TURCANTUDOR <tiudor5@gmail.com>
2016-04-16 12:03:18 +0300

    Lab5

---
+++ Lab5/libgdx/core/src/com/mygdx/game/Mygame.java
@@ -0,0 +1,34 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+/**
+ * Created by Admin on 10.04.2016.
+ */
+public class Mygame extends Game{
+    SpriteBatch batch;
+    BitmapFont font;
+
+
+    @Override
+    public void create() {
+
+        batch=new SpriteBatch();
+        font=new BitmapFont();
+        this.setScreen(new MainMenuScreen(this));
+    }
+
+    @Override
+    public void render() {
+        super.render();
+    }
+
+    @Override
+    public void dispose() {
+        super.dispose();
+        batch.dispose();
+        font.dispose();
+    }
+}
