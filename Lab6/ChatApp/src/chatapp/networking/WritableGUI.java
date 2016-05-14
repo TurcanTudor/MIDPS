ecf3a109adcf8ff931dc4eb9046b6a721f88c571
TURCANTUDOR <tiudor5@gmail.com>
2016-05-14 21:06:18 +0300

    Laboratorul 6

---
+++ Lab6/ChatApp/src/chatapp/networking/WritableGUI.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package chatapp.networking;
+
+/**
+ *
+ * @author Admin
+ */
+public interface WritableGUI {
+    
+    void write(String s);
+}
+
+
+
+
+
+
+
+
+
+
+
